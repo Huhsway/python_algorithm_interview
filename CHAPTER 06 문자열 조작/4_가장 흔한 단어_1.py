171c8f35b56a1ccbc4421bcd5e47d829753d6f29
Huhsway <seunghoe9542@naver.com>
2021-01-05 19:57:34 +0900

    # 풀이 1 리스트 컴프리헨션,  Counter 객체 사용

---
+++ CHAPTER 06 문자열 조작/4_가장 흔한 단어_1.py
@@ -0,0 +1,9 @@
+# 풀이 1 리스트 컴프리헨션,  Counter 객체 사용
+def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+    words = [word for word in re.sub(r'[^\w]', ' ',paragraph)
+        .lower().split()
+             if word not in banned]
+
+    counts = collections.Counter(words)
+    # 가장 흔하게 등장하는 단어의 첫 번째 인덱스 리턴
+    return counts.most_commot(1)[0][0]
